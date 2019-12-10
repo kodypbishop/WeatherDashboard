8f96bead12134bd23bf97ff398996fef1943c96c
Kody Bishop <55988596+kodypbishop@users.noreply.github.com>
2019-12-10 12:06:26 -0700

    used the time in seconds to get local time to make the days correct

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,7 @@ window.onload = function () {
         let url = "https://api.openweathermap.org/data/2.5/forecast?q="
         let appid = "&appid=ca120aa4d95679fffa136b2266aec98a"
         let fullURL = url + city + appid
+        // ajax call for current weather
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + appid,
             method: "GET"
@@ -20,16 +21,19 @@ window.onload = function () {
             $("#current").append(cityDate, temp, humid, wind)
 
         })
+        // ajax call for 5 day forecast
         $.ajax({
             url: fullURL,
             method: "GET"
         }).then(function (response) {
             console.log(response)
+            // creating an object to with the highs ,lows ,humidity and icons for each day of the 5 day forecast
             let forecasts = response.list
             let fiveDay = [];
             let day;
+            
             for (let i = 0; i < forecasts.length; i++) {
-                if (forecasts[i].dt_txt.includes(day)) {
+                if ((new Date(forecasts[i].dt * 1000)).toLocaleString().includes(day)) {
                     fiveDay[fiveDay.length - 1].clouds.push(forecasts[i].clouds.all);
                     fiveDay[fiveDay.length - 1].icon.push(forecasts[i].weather[0].icon);
                     fiveDay[fiveDay.length - 1].humidity.push(forecasts[i].main.humidity);
@@ -37,7 +41,9 @@ window.onload = function () {
                     fiveDay[fiveDay.length - 1].lowTemp.push(forecasts[i].main.temp_min);
                     console.log(day)
                 } else {
-                    day = forecasts[i].dt_txt.slice(0, 10);
+                    
+                    day = (new Date(forecasts[i].dt * 1000)).toLocaleString().slice(0, 10);
+                    console.log(day);
                     let obj = {
                         day: day,
                         "clouds": [forecasts[i].clouds.all],
@@ -52,11 +58,13 @@ window.onload = function () {
                 }
 
             }
-
+            // using 5 day forecast object to create html elements 
             for (let i = 0; i< 5; i++) {
+                let chosenIcon = Math.ceil(fiveDay[i].icon.length/2) - 1; 
+                console.log(chosenIcon);
                 let card = $("<div>")
                 let date = $("<div>").text(fiveDay[i].day);
-                let icon = $("<img>").attr("src","http://openweathermap.org/img/wn/" +fiveDay[i].icon[5]+"@2x.png");
+                let icon = $("<img>").attr("src","http://openweathermap.org/img/wn/" +fiveDay[i].icon[chosenIcon]+"@2x.png");
                 let high = $("<div>").text("High Tempature:" + fahrenheit(Math.max(...fiveDay[i].highTemp)).toFixed(1) + String.fromCharCode(176) + "F");
                 let low = $("<div>").text("Low Tempature:" + fahrenheit(Math.min(...fiveDay[i].lowTemp)).toFixed(1) + String.fromCharCode(176) + "F");
                 let humidity = $("<div>").text("Humidity:" +(fiveDay[i].humidity.reduce((a,b) => a + b, 0) /fiveDay[i].humidity.length).toFixed(0)+ "%");
@@ -67,6 +75,7 @@ window.onload = function () {
             let uv = "http://api.openweathermap.org/data/2.5/uvi?";
             let cor = "lat=" + response.city.coord.lat + "&lon=" + response.city.coord.lon;
             let uvURL = uv + cor + appid;
+            // ajax call for current uv index
             $.ajax({
                 url: uvURL,
                 method: "GET"
